b69f5c958095e46f6cf6b76ac6ec6e6f9e39d51d
Maheshwarirahul612 <rahulbadga51@gmail.com>
2025-05-14 08:55:52 -0700

    Initial deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Auth from './pages/Auth/Auth';
 import HomePage from './pages/Home/HomePage';
 import Navbar from './components/Navbar';
